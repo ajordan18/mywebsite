3f8700f1e39f5ea1d250cfc7f4cc5102d1ffccc6
Aaron Jordan <ajorda18@students.towson.edu>
2020-05-31 10:17:43 -0400

    Added test console.log functions to index.js

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+function alert(){
+    console.log('THIS IS AN ALERT!');
+}; 
+
+function buttonAlert(){
+    console.log('BUTTON PRESSED!');
+};
+
+function add(){
+    console.log(1 + 2 + 3);
+};

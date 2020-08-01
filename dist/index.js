d9595f6090a1813c9ff26372239c49030bdd246d
Aaron Jordan <ajorda18@students.towson.edu>
2020-08-01 12:55:26 -0400

    Added output display for calculator section

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,9 @@ function buttonAlert(){
 function add(){
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    let result = num1 + num2;
-    console.log(result);
+    let sum = num1 + num2;
+
+    let result = document.getElementById("result");
+    result.innerHTML = sum;
+    console.log(sum);
 };
